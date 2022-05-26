aaba3cab12647b7f63c220aaede4837a962c44e3
Alisa Pavlova <alipavlova2020@gmail.com>
2022-05-26 22:53:42 +0300

    Test1

---
+++ src/test/Test1.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int amount = 89;
+        System.out.println("стоимость билета = " + amount);
+        int mili = 20;
+        System.out.println("за каждые " +
+                mili +
+                " рублей 1 миля");
+        System.out.println("всего бонусных миль" + amount / mili
+        );
+    }
+}
